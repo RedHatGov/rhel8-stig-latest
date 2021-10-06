49efae3d6359601a245e617cba759030288e8010
Automated Publisher <actions@users.noreply.github.com>
2021-10-06 00:26:12 +0000

    Automated publish: Wed Oct  6 00:26:12 UTC 2021 4b0bada0eab64a590c0159d7e4e6ecd6b09698bc

--- rhel8-script-stig.sh
+++ rhel8-script-stig.sh
@@ -2624,14 +2624,34 @@ fi
 # BEGIN fix (100 / 364) for 'accounts_password_set_max_life_existing'
 ###############################################################################
 (>&2 echo "Remediating rule 100/364: 'accounts_password_set_max_life_existing'")
-# FIX FOR THIS RULE IS MISSING
+#!/bin/bash
+
+
+var_accounts_maximum_age_login_defs="60"
+
+
+usrs_max_pass_age=( $(awk -v var="$var_accounts_maximum_age_login_defs" -F: '$5 > var || $5 == "" {print $1}' /etc/shadow) )
+for i in ${usrs_max_pass_age[@]};
+do
+  passwd -x $var_accounts_maximum_age_login_defs $i
+done
 # END fix for 'accounts_password_set_max_life_existing'
 
 ###############################################################################
 # BEGIN fix (101 / 364) for 'accounts_password_set_min_life_existing'
 ###############################################################################
 (>&2 echo "Remediating rule 101/364: 'accounts_password_set_min_life_existing'")
-# FIX FOR THIS RULE IS MISSING
+#!/bin/bash
+
+
+var_accounts_minimum_age_login_defs="1"
+
+
+usrs_min_pass_age=( $(awk -v var="$var_accounts_minimum_age_login_defs" -F: '$4 < var || $4 == "" {print $1}' /etc/shadow) )
+for i in ${usrs_min_pass_age[@]};
+do
+  passwd -n $var_accounts_minimum_age_login_defs $i
+done
 # END fix for 'accounts_password_set_min_life_existing'
 
 ###############################################################################
