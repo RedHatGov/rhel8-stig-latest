dc18a21d5806fd55a83db49fb1b8abd0a0c48c7c
Automated Publisher <actions@users.noreply.github.com>
2021-11-25 00:26:01 +0000

    Automated publish: Thu Nov 25 00:26:01 UTC 2021 40f14eb2f2f8a9d94fca2cd0133dbc1dc3911758

--- rhel8-script-stig.sh
+++ rhel8-script-stig.sh
@@ -28965,6 +28965,10 @@ fi
 firewalld_sshd_zone='public'
 
 
+
+  {% set network_config_path = "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}" %}
+
+
 # This assumes that firewalld_sshd_zone is one of the pre-defined zones
 if [ ! -f /etc/firewalld/zones/${firewalld_sshd_zone}.xml ]; then
     cp /usr/lib/firewalld/zones/${firewalld_sshd_zone}.xml /etc/firewalld/zones/${firewalld_sshd_zone}.xml
@@ -28978,7 +28982,7 @@ fi
 nic_bound=false
 eth_interface_list=$(ip link show up | cut -d ' ' -f2 | cut -d ':' -s -f1 | grep -E '^(en|eth)')
 for interface in $eth_interface_list; do
-    if grep -q "ZONE=$firewalld_sshd_zone" /etc/sysconfig/network-scripts/ifcfg-$interface; then
+    if grep -qi "ZONE=$firewalld_sshd_zone" ; then
         nic_bound=true
         break;
     fi
@@ -28988,10 +28992,11 @@ if [ $nic_bound = false ];then
     # Add first NIC to SSH enabled zone
 
     if ! firewall-cmd --state -q; then
-        # Test if the config_file is a symbolic link. If so, use --follow-symlinks with sed.
+        
+          # Test if the config_file is a symbolic link. If so, use --follow-symlinks with sed.
         # Otherwise, regular sed command will do.
         sed_command=('sed' '-i')
-        if test -L "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}"; then
+        if test -L ""; then
             sed_command+=('--follow-symlinks')
         fi
 
@@ -29005,17 +29010,18 @@ if [ $nic_bound = false ];then
         # If the key exists, change it. Otherwise, add it to the config_file.
         # We search for the key string followed by a word boundary (matched by \>),
         # so if we search for 'setting', 'setting2' won't match.
-        if LC_ALL=C grep -q -m 1 -i -e "^ZONE=\\>" "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}"; then
-            "${sed_command[@]}" "s/^ZONE=\\>.*/$formatted_output/gi" "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}"
+        if LC_ALL=C grep -q -m 1 -i -e "^ZONE=\\>" ""; then
+            "${sed_command[@]}" "s/^ZONE=\\>.*/$formatted_output/gi" ""
         else
             # \n is precaution for case where file ends without trailing newline
             cce="CCE-84300-3"
-            printf '\n# Per %s: Set %s in %s\n' "$cce" "$formatted_output" "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}" >> "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}"
-            printf '%s\n' "$formatted_output" >> "/etc/sysconfig/network-scripts/ifcfg-${eth_interface_list[0]}"
+            printf '\n# Per %s: Set %s in %s\n' "$cce" "$formatted_output" "" >> ""
+            printf '%s\n' "$formatted_output" >> ""
         fi
+        
     else
         # If firewalld service is running, we need to do this step with firewall-cmd
-        # Otherwise firewalld will comunicate with NetworkManage and will revert assigned zone
+        # Otherwise firewalld will communicate with NetworkManage and will revert assigned zone
         # of NetworkManager managed interfaces upon reload
         firewall-cmd --permanent --zone=$firewalld_sshd_zone --add-interface=${eth_interface_list[0]}
         firewall-cmd --reload
