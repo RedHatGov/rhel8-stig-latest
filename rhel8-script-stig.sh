c4b4d4b92b15f251cbf275e8eecb7ce71d3847a5
Automated Publisher <actions@users.noreply.github.com>
2022-04-30 00:08:55 +0000

    Automated publish: Sat Apr 30 00:08:55 UTC 2022 549015dbc66acfa17800134802e569032b362fd2

--- rhel8-script-stig.sh
+++ rhel8-script-stig.sh
@@ -440,8 +440,12 @@ OPENSSL_CRYPTO_POLICY_SECTION_REGEX='\[\s*crypto_policy\s*\]'
 OPENSSL_CRYPTO_POLICY_INCLUSION='.include /etc/crypto-policies/back-ends/opensslcnf.config'
 OPENSSL_CRYPTO_POLICY_INCLUSION_REGEX='^\s*\.include\s*/etc/crypto-policies/back-ends/opensslcnf.config$'
 
+
+  
+
+
 function remediate_openssl_crypto_policy() {
-	CONFIG_FILE="/etc/pki/tls/openssl.cnf"
+	CONFIG_FILE=/etc/pki/tls/openssl.cnf
 	if test -f "$CONFIG_FILE"; then
 		if ! grep -q "^\\s*$OPENSSL_CRYPTO_POLICY_SECTION_REGEX" "$CONFIG_FILE"; then
 			printf '\n%s\n\n%s' "$OPENSSL_CRYPTO_POLICY_SECTION" "$OPENSSL_CRYPTO_POLICY_INCLUSION" >> "$CONFIG_FILE"
