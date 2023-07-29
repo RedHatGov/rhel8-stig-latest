43aee21de28cd517e2dd0637d769bac1bf5c973e
Automated Publisher <actions@users.noreply.github.com>
2023-07-29 00:09:59 +0000

    Automated publish: Sat Jul 29 00:09:59 UTC 2023 702090366aa9ab4733143a27e353e01e9f70eeb0

--- rhel8-script-stig.sh
+++ rhel8-script-stig.sh
@@ -32774,19 +32774,13 @@ fi
 # Remediation is applicable only in certain platforms
 if [ ! -f /.dockerenv ] && [ ! -f /run/.containerenv ]; then
 
+SOCKET_NAME="systemd-coredump.socket"
 SYSTEMCTL_EXEC='/usr/bin/systemctl'
-"$SYSTEMCTL_EXEC" stop 'systemd-coredump.service'
-"$SYSTEMCTL_EXEC" disable 'systemd-coredump.service'
-"$SYSTEMCTL_EXEC" mask 'systemd-coredump.service'
-# Disable socket activation if we have a unit file for it
-if "$SYSTEMCTL_EXEC" -q list-unit-files systemd-coredump.socket; then
-    "$SYSTEMCTL_EXEC" stop 'systemd-coredump.socket'
-    "$SYSTEMCTL_EXEC" mask 'systemd-coredump.socket'
+
+if "$SYSTEMCTL_EXEC" -q list-unit-files --type socket | grep -q "$SOCKET_NAME"; then
+    "$SYSTEMCTL_EXEC" stop "$SOCKET_NAME"
+    "$SYSTEMCTL_EXEC" mask "$SOCKET_NAME"
 fi
-# The service may not be running because it has been started and failed,
-# so let's reset the state so OVAL checks pass.
-# Service should be 'inactive', not 'failed' after reboot though.
-"$SYSTEMCTL_EXEC" reset-failed 'systemd-coredump.service' || true
 
 else
     >&2 echo 'Remediation is not applicable, nothing was done'
