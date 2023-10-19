77c53bd1fdf1955c8c83967dff6518cbc83893ce
Automated Publisher <actions@users.noreply.github.com>
2023-10-19 00:08:59 +0000

    Automated publish: Thu Oct 19 00:08:59 UTC 2023 cc8fee4517fc009a3376a56bb3756469fba79b20

--- rhel8-script-stig.sh
+++ rhel8-script-stig.sh
@@ -33290,7 +33290,37 @@ fi
 # BEGIN fix (349 / 403) for 'xccdf_org.ssgproject.content_rule_fapolicy_default_deny'
 ###############################################################################
 (>&2 echo "Remediating rule 349/403: 'xccdf_org.ssgproject.content_rule_fapolicy_default_deny'")
-(>&2 echo "FIX FOR THIS RULE 'xccdf_org.ssgproject.content_rule_fapolicy_default_deny' IS MISSING!")
+# Remediation is applicable only in certain platforms
+if [ ! -f /.dockerenv ] && [ ! -f /run/.containerenv ]; then
+
+cat > /etc/fapolicyd/rules.d/99-deny-everything.rules << EOF
+# Red Hat KCS 7003854 (https://access.redhat.com/solutions/7003854)
+deny perm=any all : all
+EOF
+
+chmod 644 /etc/fapolicyd/rules.d/99-deny-everything.rules
+chgrp fapolicyd /etc/fapolicyd/rules.d/99-deny-everything.rules
+
+if [ -e "/etc/fapolicyd/fapolicyd.conf" ] ; then
+    
+    LC_ALL=C sed -i "/^\s*permissive\s*=\s*/Id" "/etc/fapolicyd/fapolicyd.conf"
+else
+    touch "/etc/fapolicyd/fapolicyd.conf"
+fi
+# make sure file has newline at the end
+sed -i -e '$a\' "/etc/fapolicyd/fapolicyd.conf"
+
+cp "/etc/fapolicyd/fapolicyd.conf" "/etc/fapolicyd/fapolicyd.conf.bak"
+# Insert at the end of the file
+printf '%s\n' "permissive = 0" >> "/etc/fapolicyd/fapolicyd.conf"
+# Clean up after ourselves.
+rm "/etc/fapolicyd/fapolicyd.conf.bak"
+
+systemctl restart fapolicyd
+
+else
+    >&2 echo 'Remediation is not applicable, nothing was done'
+fi
 
 # END fix for 'xccdf_org.ssgproject.content_rule_fapolicy_default_deny'
 
