556ea0e4aea62b27f041218886d8c0350d8ae2c6
Automated Publisher <actions@users.noreply.github.com>
2024-01-10 00:06:56 +0000

    Automated publish: Wed Jan 10 00:06:56 UTC 2024 8de13a0885b7b2f421af802339526b572f2b7fa0

--- rhel8-script-stig.sh
+++ rhel8-script-stig.sh
@@ -31111,7 +31111,19 @@ if ! rpm -q --quiet "NetworkManager" ; then
     yum install -y "NetworkManager"
 fi
 
-nmcli radio all off
+if command -v nmcli >/dev/null 2>&1 && systemctl is-active NetworkManager >/dev/null 2>&1; then
+    nmcli radio all off
+fi
+
+if command -v wicked >/dev/null 2>&1 && systemctl is-active wickedd >/dev/null 2>&1; then
+  if [ -n "$(find /sys/class/net/*/ -type d -name wireless)" ]; then
+    interfaces=$(find /sys/class/net/*/wireless -type d -name wireless | xargs -0 dirname | xargs basename)
+    for iface in $interfaces; do
+      wicked ifdown $iface
+      sed -i 's/STARTMODE=.*/STARTMODE=off/' /etc/sysconfig/network/ifcfg-$iface
+    done
+  fi
+fi
 
 # END fix for 'xccdf_org.ssgproject.content_rule_wireless_disable_interfaces'
 
